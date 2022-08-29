50c2cc80e92a3e51ba155c395c63dbe95b49cb8f
willydekeyser <willy.de.keyser@skynet.be>
2022-08-29 16:41:02 +0200

    Add Controller

---
+++ SpringBootSecuritySamplesPart1/src/main/java/willydekeyser/controller/Controller.java
@@ -0,0 +1,13 @@
+package willydekeyser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String welkom() {
+		return "Welkom";
+	}
+}
