42e7d5070fcf394b77c652f0c419098799a24154
willydekeyser <willy.de.keyser@skynet.be>
2022-08-30 20:51:16 +0200

    Setup SpringBootSecuritySamplesPart5

---
+++ SpringBootSecuritySamplesPart5/src/main/java/willydekeyser/SpringBootSecuritySamplesPart5Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuritySamplesPart5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuritySamplesPart5Application.class, args);
+	}
+
+}
